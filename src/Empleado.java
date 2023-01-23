ca66e5572fa20bc0066b3cdaad97b9aff91c6b93
AndersonElit <rodriarri@gmail.com>
2023-01-22 22:12:44 -0500

    creacion clase empleado

---
+++ src/Empleado.java
@@ -0,0 +1,25 @@
+public class Empleado {
+    private Integer id;
+    private String nombre;
+
+    public Empleado(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
